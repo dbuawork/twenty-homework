eb0b4f3ccb4c7378f2a4f06679dcbd2223c516c3
dbuawork <dbuawork@gmail.com>
2024-01-19 08:43:41 +0200

    second-task-done question

--- main.py
+++ main.py
@@ -26,13 +26,55 @@ class City:
         return self._phone_code
 
 
+class Country:
+    def __init__(self, name, continent, population, phone_code, capital):
+        self._name = name
+        self._continent = continent
+        self._population = population
+        self._phone_code = phone_code
+        self._capital = capital
+        self._cities = []
+
+    def get_name(self):
+        return self._name
+
+    def get_continent(self):
+        return self._continent
+
+    def get_population(self):
+        return self._population
+
+    def get_phone_code(self):
+        return self._phone_code
+
+    def get_capital(self):
+        return self._capital
+
+    def get_cities(self):
+        return self._cities
+
+    def add_city(self, city):
+        self._cities.append(city)
+
+    def remove_city(self, city_name):
+        self._cities = [city for city in self._cities if city.get_name() != city_name]
+
+
 # Приклад використання
 city1 = City("Київ", "Київська область", "Україна", 2800000, "02000", "+380")
+city2 = City("Львів", "Львівська область", "Україна", 720000, "79000", "+380")
 
-# Доступ до полів через методи
-print(f"Назва міста: {city1.get_name()}")
-print(f"Регіон: {city1.get_region()}")
-print(f"Країна: {city1.get_country()}")
-print(f"Населення: {city1.get_population()}")
-print(f"Поштовий індекс: {city1.get_postal_code()}")
-print(f"Телефонний код: {city1.get_phone_code()}")
+cities_in_country = [city1, city2]
+
+country = Country("Україна", "Європа", 42000000, "+380", "Київ")
+
+country.add_city(city1)
+country.add_city(city2)
+
+print(f"Назва країни: {country.get_name()}")
+print(f"Кількість жителів: {country.get_population()}")
+print(f"Столиця: {country.get_capital()}")
+
+print("\nМіста в країні:")
+for city in country.get_cities():
+    print(f"- {city.get_name()}, Населення: {city.get_population()}, Поштовий індекс: {city.get_postal_code()}, Телефонний код: {city.get_phone_code()}")
